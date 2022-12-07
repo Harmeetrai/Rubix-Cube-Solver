72133f3f8eeb74e8d1ef9a1b28994102669dbf2c
Harmeetrai <harmeetrai18@gmail.com>
2022-12-07 15:24:20 -0800

    update main

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-import rubix_cube import RubixCube
+from rubix_cube import RubixCube
+
+rubix_cube = RubixCube(n=3)
+rubix_cube.print_cube()
+
+# shuffle the cube
+rubix_cube.shuffle()
+
+print()
+rubix_cube.print_cube()
+
+# solver the cube
+# rubix_cube.solve()
