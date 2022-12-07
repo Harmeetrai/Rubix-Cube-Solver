1d727b80662ae09409ab679edad25d4016e7d198
Harmeetrai <harmeetrai18@gmail.com>
2022-12-07 15:27:41 -0800

    create test file

---
+++ test.py
@@ -0,0 +1,12 @@
+# test the functionally of the solver by comparing the output of the solver with the solved cube
+from rubix_cube import RubixCube
+
+rubix_cube = RubixCube(n=3)
+rubix_cube.print_cube()
+
+# test cases
+def test_is_solved():
+    assert rubix_cube.is_solved() == 1
+    rubix_cube.solve()
+    assert rubix_cube.is_solved() == 0
+    
